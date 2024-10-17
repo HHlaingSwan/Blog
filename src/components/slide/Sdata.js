ad66605180f948ebd6afc47b79661b2e896a2688
HHlaingSwan <168323270+HHlaingSwan@users.noreply.github.com>
2024-10-17 10:38:02 +0630

    image err fix

--- src/components/slide/Sdata.js
+++ src/components/slide/Sdata.js
@@ -1,21 +1,21 @@
 const Sdata = [
   {
     id: 1,
-    cover: "/Images/image-9.webp",
+    cover: "Images/image-9.webp",
     category: "lifestyle",
     title: "What are the best countries to visit?",
     desc: "In this post, we are going to highlight the many benefits of blogging for business and how you can get started with",
   },
   {
     id: 2,
-    cover: "/Images/image-10.webp",
+    cover: "Images/image-10.webp",
     category: "lifestyle",
     title: "What are the best countries to visit?",
     desc: "In this post, we are going to highlight the many benefits of blogging for business and how you can get started with",
   },
   {
     id: 3,
-    cover: "/Images/image-8.webp",
+    cover: "Images/image-8.webp",
     category: "lifestyle",
     title: "What are the best countries to visit?",
     desc: "In this post, we are going to highlight the many benefits of blogging for business and how you can get started with",
